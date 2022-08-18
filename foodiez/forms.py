5a26430e0548bc424adb2fad954652aad19e84e6
SlimanovQ8 <Slimanovq8@gmail.com>
2022-08-19 00:34:12 +0300

    first commit

---
+++ foodiez/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from food import models
+from food.models import Category, Ingredient
+
+User = get_user_model()
+
+
+class Registerform(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', "first_name", "email"]
+        widgets ={
+            "password": forms.PasswordInput()
+        }
+class UserLogin(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
+
+class CategoryForm(forms.ModelForm):
+
+
+    class Meta:
+        model = models.Category
+        exclude = ["created_by"]
+
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = models.Ingredient
+        fields = "__all__"
+        widgets = {
+            'name': forms.TextInput(attrs= {'class': 'form-control'}),
+            # 'image': forms.ImageField(attrs={'class': 'form-control'}),
+            'category_name': forms.Select(attrs={'class': 'form-control'}),
+        },
+
+
+
+
+
+class RecipeForm(forms.ModelForm):
+
+
+    class Meta:
+        model = models.Recipe
+        exclude = ["created_by"]
+        widgets = {
+            'ingredients': forms.CheckboxSelectMultiple,
+        }
+
+
